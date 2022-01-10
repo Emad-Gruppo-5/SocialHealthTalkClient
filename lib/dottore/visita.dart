668f6cb535e5a899e0b9bce439592c114556f329
mflora4 <m.flora4@studenti.unisa.it>
2022-01-10 21:57:22 +0100

    no pop from visit details to visita in doctor

--- lib/dottore/visita.dart
+++ lib/dottore/visita.dart
@@ -28,79 +28,47 @@ class createVisitaState extends State<createVisita> {
   }
 
   String _selectedDate = '';
-  String _dateCount = '';
-  String _range = '';
-  String _rangeCount = '';
 
   void _onSelectionChanged(DateRangePickerSelectionChangedArgs args) {
     setState(() {
-      if (args.value is PickerDateRange) {
-        _range = '${DateFormat('dd/MM/yyyy').format(args.value.startDate)} -'
-            // ignore: lines_longer_than_80_chars
-            ' ${DateFormat('dd/MM/yyyy').format(args.value.endDate ?? args.value.startDate)}';
-      } else if (args.value is DateTime) {
+      if (args.value is DateTime) {
         _selectedDate = args.value.toString();
-      } else if (args.value is List<DateTime>) {
-        _dateCount = args.value.length.toString();
-      } else {
-        _rangeCount = args.value.length.toString();
       }
     });
-    Navigator.push(
-        context,
-        MaterialPageRoute(
-            builder: (context) => TimePickerVisita(
-                cod_fiscale: cod_fiscale, token: token, date: _selectedDate)));
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        localizationsDelegates: [
+        localizationsDelegates: const [
           GlobalMaterialLocalizations.delegate,
           GlobalWidgetsLocalizations.delegate,
         ],
-        supportedLocales: [
-          const Locale('it'),
+        supportedLocales: const [
+          Locale('it'),
         ],
         locale: const Locale('it'),
         home: Scaffold(
             appBar: AppBar(
               title: const Text("Pianifica visita"),
+              actions: [
+                IconButton(
+                    onPressed: () => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => TimePickerVisita(
+                                cod_fiscale: cod_fiscale,
+                                token: token,
+                                date: _selectedDate))),
+                    icon: const Icon(Icons.add)),
+              ],
             ),
             body: Stack(
               children: <Widget>[
-                Positioned(
-                  left: 0,
-                  right: 0,
-                  top: 0,
-                  height: 80,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Center(
-                          child: Text(
-                        'Seleziona un giorno',
-                        style: const TextStyle(fontSize: 30),
-                      )),
-                    ],
-                  ),
+                SfDateRangePicker(
+                  onSelectionChanged: _onSelectionChanged,
+                  selectionMode: DateRangePickerSelectionMode.single,
                 ),
-                Positioned(
-                  left: 0,
-                  top: 80,
-                  right: 0,
-                  bottom: 0,
-                  child: SfDateRangePicker(
-                    onSelectionChanged: _onSelectionChanged,
-                    selectionMode: DateRangePickerSelectionMode.single,
-                    initialSelectedRange: PickerDateRange(
-                        DateTime.now().subtract(const Duration(days: 4)),
-                        DateTime.now().add(const Duration(days: 3))),
-                  ),
-                )
               ],
             )));
   }
