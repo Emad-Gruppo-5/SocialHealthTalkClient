36b0a07cc5075ac590b52e1411657d18c7fe4aac
mflora4 <m.flora4@studenti.unisa.it>
2022-02-14 14:50:09 +0100

    url server

--- lib/costanti.dart
+++ lib/costanti.dart
@@ -1,5 +1,4 @@
-var urlServer = '192.168.1.55';
+var urlServer = 'https://socialhealthtalk.herokuapp.com/';
 var online_dur = 15;
 var alert_dur = 30;
 var question_dur = 1000;
-// var urlServer = 'https://socialhealthtalk.herokuapp.com/';
